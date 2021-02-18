0ef82d4a00bf5009fd0aee4a2ac3d4a954143b18
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-02-18 10:25:37 -0300

    first

---
+++ src/test/java/com/bootcamp/desafioquality/controller/BookingControllerTest.java
@@ -0,0 +1,15 @@
+package com.bootcamp.desafioquality.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingControllerTest {
+
+    @Test
+    void test() {
+        
+    }
+
+}
